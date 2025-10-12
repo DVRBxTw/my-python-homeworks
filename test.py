d4e8a2dcdeb7ce5577acfa2fe8e5daebd073a927
廖宇宥 <106516946+DVRBxTw@users.noreply.github.com>
2025-10-12 16:20:36 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,26 +1,77 @@
-content = input().split() #假設 1 + 2 * 3 - 4 / 5 會變成["1","+","2","*","3","-","4","/","5"]
-i,result = 0,0.0
-while (i < len(content)): #判斷i的範圍是否在content的list長度內
-    if(content[i] == '*'):
-        content[i-1] = float(content[i-1]) * float(content[i+1]) #因為index偵測在index = 3 的時候是*號 所以會變成["1","+","6.0","*","3","-","4","/","5"]
-        del content[i:i+2] #把*跟3刪除 ["1","+","6.0","-","4","/","5"]
-        i-=1 #index倒退1位 (從原本指向index 3變成 index 2) 為了判斷運算子
-    elif(content[i] == '/'):
-        content[i-1] = float(content[i-1]) / float(content[i+1]) #因為index偵測在index = 6 的時候是/號 所以會變成["1","+","6.0","-",0.8","/","5"]
-        del content[i:i+2] #把/跟5刪除 ["1","+","6.0","-","0.8"]
-        i-=1 #index倒退1位 (從原本指向index 6變成 index 5) 為了判斷運算子
-    i+=1
-i = 0
-while(i<len(content)):
-    if(content[i] == '+'):
-        content[i-1] = float(content[i-1]) + float(content[i+1])
-        del content[i:i+2]
-        i-=1
-    elif(content[i] == '-'):
-        content[i-1] = float(content[i-1]) - float(content[i+1])
-        del content[i:i+2]
-        i-=1
-    i+=1
-print('%.2f' % float(content[0]))
+pattern = input().split() #把pattern的輸入 例如：S D D C H --> ["S","D","D","C","H"]
+card = input().split() #A K 5 Q J
+card_point,index,bonus_point = 0,0,0
+maximum = []
+stoper = set()
+temp = ''
+#判斷建議行動
+S_count,H_count,D_count,C_count = pattern.count("S"),pattern.count("H"),pattern.count("D"),pattern.count("C")
+maxi = max(S_count,H_count,D_count,C_count)
+count = {"S":S_count,"H":H_count,"D":D_count,"C":C_count} 
+for i,j in count.items():
+    if j == maxi:
+        maximum.append(i)
+if(len(maximum) > 1):
+    if("S" in maximum):
+        action = "Spade"
+    elif("H" in maximum):
+        action = "Heart"
+    elif("D" in maximum):
+        action = "Diamond"
+    elif("C" in maximum):
+        action = "Club"
+else:
+    if("S" in maximum):
+        action = "Spade"
+    elif("H" in maximum):
+        action = "Heart"
+    elif("D" in maximum):
+        action = "Diamond"
+    elif("C" in maximum):
+        action = "Club"
+    
+#轉換點數 及 計算總點力
+if len(set(pattern)) == 1:
+    bonus_point = 2
+for i in card:
+    if(i == "A"):
+        card_point += 4
+    elif(i == 'K'):
+        card_point += 3
+    elif(i == 'Q'):
+        card_point += 2
+    elif(i == 'J'):
+        card_point += 1
+total_point = card_point + bonus_point
 
-        
+
+if('A' in card):
+    for i,j in zip(pattern,card):
+        if(j == 'A'):
+            stoper.add(i)
+if('K' in card):
+    temp = ''
+    for i,j in zip(pattern,card):
+        if(j == 'K'):
+            temp += i
+            if len(temp) > 1:
+                stoper.add(temp)
+
+if('Q' in card):
+    temp = ''
+    for i,j in zip(pattern,card):
+        if(j == 'Q'):
+            temp += i
+            if len(temp) > 2:
+                stoper.add(temp)
+stop = list(stoper)
+
+
+if(total_point < 8):
+    suggest = "Pass"
+elif(total_point >= 15):
+    suggest = "Strong Open"
+elif(total_point >= 8 and total_point <= 14):
+    suggest = f"Open {action}"
+
+print(f"HCP: {card_point}\nTotal Points: {total_point}\nDistribution (S-H-D-C):{S_count}-{H_count}-{D_count}-{C_count}\nStopped Suits: {stop}\nOpening Bid: {suggest}")
